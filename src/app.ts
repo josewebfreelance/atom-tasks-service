ee07c0125b5f7932c1484519a91411310ade83d7
JoseLemus <jlemus@is4tech.com>
2024-10-14 15:16:44 -0600

    API TASKS  - RUTA  - CONTROLADOR  - SERVICIO  - MODELO

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import configRoute from "./routes/configRoute";
 import securityRoute from "./routes/securityRoute";
+import taskRoute from "./routes/taskRoute";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
@@ -12,5 +13,6 @@ app.use(cors());
 app.use(express.json());
 app.use(configRoute);
 app.use(securityRoute);
+app.use(taskRoute);
 
 app.listen(+PORT, () => console.log(`Process started in ${+PORT}`))
