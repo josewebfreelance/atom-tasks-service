76d3b5ecdd3a84ac1a66a857be726b0580ffca6d
JoseLemus <jlemus@is4tech.com>
2024-10-14 14:22:21 -0600

    API CONFIGURACION LOGIN  - RUTA  - CONTROLADOR  - SERVICIO

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import configRoute from "./routes/configRoute";
+import securityRoute from "./routes/securityRoute";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
@@ -10,5 +11,6 @@ const app = express()
 app.use(cors());
 app.use(express.json());
 app.use(configRoute);
+app.use(securityRoute);
 
 app.listen(+PORT, () => console.log(`Process started in ${+PORT}`))
